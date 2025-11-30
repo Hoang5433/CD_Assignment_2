9f95df345e0f07985108962ee599bbeee24598ca
Phung Thanh Do <hoangquy3001@gmail.com>
2025-11-30 20:42:33 +0700

    fix: card.jsx

--- FloginFE_BE/frontend/src/components/ui/card.jsx
+++ FloginFE_BE/frontend/src/components/ui/card.jsx
@@ -23,17 +23,23 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-    <h2
-        ref={ref}
-        className={cn(
-            "text-2xl font-semibold leading-none tracking-tight",
-            className
-        )}
-        {...props}
-    />
-))
-CardTitle.displayName = "CardTitle"
+const CardTitle = React.forwardRef(
+    ({ className, children, ...props }, ref) => (
+        <h2
+            ref={ref}
+            className={cn(
+                "text-2xl font-semibold leading-none tracking-tight",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </h2>
+    )
+);
+
+CardTitle.displayName = "CardTitle";
+
 
 const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
     <p
